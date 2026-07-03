255b0f864e07370dc41ab6dc3dcc941f5bf2e5c5
arya123224 <kharsh24485@gmail.com>
2026-07-03 11:34:06 +0530

    ✅ solve: 15. 3Sum [Medium] - Accepted (316/316 test cases)

---
+++ Medium/15-3Sum/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1, right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
+/*
+ * Problem: 15. 3Sum
+ * Difficulty: Medium
+ * Runtime: 35ms | Beats 44.04%
+ * Memory: 59.17 MB | Beats 59.69%
+ * Status: Accepted - 316/316 test cases passed
+ * Date: 2026-07-02
+ * Link: https://leetcode.com/problems/3sum/
+ */
+
